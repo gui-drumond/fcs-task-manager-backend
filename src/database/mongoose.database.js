2fd053f8dc15adb2761008e68ce4c7e4e2a788d0
Guilherme Drumond <guilherme.drumond@ciandt.com>
2023-08-22 14:18:16 -0300

    Feat: Connection to mongoDb

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-    await mongoose.connect(
-        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fsctaskmanagercluster.6wsaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-        (error) => {
+    await mongoose
+        .connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fsctaskmanagercluster.wao625t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+        )
+        .then((res) => console.log("Connected to MongoDB!"))
+        .catch((error) => {
             if (error) {
                 return console.log(
                     `Could not connect to MongoDB: ${error.message}`
                 );
             }
-
-            return console.log("Connected to MongoDB!");
-        }
-    );
+        });
 };
 
 module.exports = connectToDatabase;
