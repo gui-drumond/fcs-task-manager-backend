4cf98e5215c476d51bb79d7fd79e1b812180d674
Guilherme Drumond <guilherme.drumond@ciandt.com>
2023-08-25 11:31:59 -0300

    Feat: add router file to task and finished CRUD

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 const connectToDataBase = require("./src/database/mongoose.database");
+const TaskRouter = require("./src/routes/task.routes");
+
 dotenv.config();
 const app = express();
 app.use(express.json());
-const TaskModel = require("./src/models/task.model");
-
 connectToDataBase();
-app.get("/tasks", async (req, res) => {
-    const tasks = await TaskModel.find({});
-    res.status(200).send(tasks);
-});
 
-app.post("/tasks", async (req, res) => {
-    try {
-        const newTask = new TaskModel(req.body);
-        await newTask.save();
-        res.status(201).send(newTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+app.use("/tasks", TaskRouter);
 
 app.listen("3000", () => {
     console.log("Rodando o servidor ");
