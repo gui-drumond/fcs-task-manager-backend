71e77305f84d3f1970930bd410588fa1633b74c2
Guilherme Drumond <guilherme.drumond@ciandt.com>
2023-08-29 15:11:50 -0300

    Feat: add cors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const connectToDataBase = require("./src/database/mongoose.database");
 const TaskRouter = require("./src/routes/task.routes");
 
 dotenv.config();
 const app = express();
-app.use(express.json());
 connectToDataBase();
 
+app.use(express.json());
+app.use(cors());
 app.use("/tasks", TaskRouter);
 
 app.listen("3000", () => {
